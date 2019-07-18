ec4e985e60748fe7726a0b707284d534a9564c90
kyleb <kyleb3nko@gmail.com>
2019-07-18 10:25:23 +0200

    create new album

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Header, Input, List, Segment } from 'semantic-ui-react';
-import Amplify, { graphqlOperation } from 'aws-amplify';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import aws_exports from './aws-exports';
 import { withAuthenticator, Connect } from 'aws-amplify-react';
 
@@ -76,6 +76,56 @@ class AlbumsListLoader extends React.Component {
 }
 
 
+// 2. NEW: Create a NewAlbum component to give us 
+//    a way of saving new albums
+class NewAlbum extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      albumName: ''
+    };
+  }
+
+  handleChange = (event) => {
+    let change = {};
+    change[event.target.name] = event.target.value;
+    this.setState(change);
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const NewAlbum = `mutation NewAlbum($name: String!) {
+      createAlbum(input: {name: $name}) {
+        id
+        name
+      }
+    }`;
+
+    const result = await API.graphql(graphqlOperation(NewAlbum, { name: this.state.albumName }));
+    console.info(`Created album with id ${result.data.createAlbum.id}`);
+  }
+
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>Add a new album</Header>
+        <Input
+          type='text'
+          placeholder='New Album Name'
+          icon='plus'
+          iconPosition='left'
+          action={{ content: 'Create', onClick: this.handleSubmit }}
+          name='albumName'
+          value={this.state.albumName}
+          onChange={this.handleChange}
+        />
+      </Segment>
+    )
+  }
+}
+
+
+
 // 6. EDIT: Change the App component to look nicer and
 //    use the AlbumsListLoader component
 class App extends Component {
@@ -83,6 +133,7 @@ class App extends Component {
     return (
       <Grid padded>
         <Grid.Column>
+          <NewAlbum />
           <AlbumsListLoader />
         </Grid.Column>
       </Grid>
