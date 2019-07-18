faec8bbd4c8794a3e311c52018ed4b600067619c
kyleb <kyleb3nko@gmail.com>
2019-07-18 10:00:38 +0200

    see albums

--- src/App.js
+++ src/App.js
@@ -1,17 +1,91 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
-import Amplify from 'aws-amplify';
+import { Grid, Header, Input, List, Segment } from 'semantic-ui-react';
+import Amplify, { graphqlOperation } from 'aws-amplify';
 import aws_exports from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react';
+import { withAuthenticator, Connect } from 'aws-amplify-react';
 
 Amplify.configure(aws_exports);
 
+// 2. NEW: Create a function we can use to 
+//    sort an array of objects by a common property
+function makeComparator(key, order = 'asc') {
+  return (a, b) => {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+
+    const aVal = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const bVal = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
+
+    return order === 'desc' ? (comparison * -1) : comparison
+  };
+}
+
+
+// 3. NEW: Add an AlbumsList component for rendering 
+//    a sorted list of album names
+class AlbumsList extends React.Component {
+  albumItems() {
+    return this.props.albums.sort(makeComparator('name')).map(album =>
+      <li key={album.id}>
+        {album.name}
+      </li>);
+  }
+
+  render() {
+    return (
+      <Segment>
+        <Header as='h3'>My Albums</Header>
+        <List divided relaxed>
+          {this.albumItems()}
+        </List>
+      </Segment>
+    );
+  }
+}
+
+
+// 4. NEW: Add a new string to query all albums
+const ListAlbums = `query ListAlbums {
+    listAlbums(limit: 9999) {
+        items {
+            id
+            name
+        }
+    }
+}`;
+
+
+// 5. NEW: Add an AlbumsListLoader component that will use the 
+//    Connect component from Amplify to provide data to AlbumsList
+class AlbumsListLoader extends React.Component {
+  render() {
+    return (
+      <Connect query={graphqlOperation(ListAlbums)}>
+        {({ data, loading, errors }) => {
+          if (loading) { return <div>Loading...</div>; }
+          if (!data.listAlbums) return;
+
+          return <AlbumsList albums={data.listAlbums.items} />;
+        }}
+      </Connect>
+    );
+  }
+}
+
+
+// 6. EDIT: Change the App component to look nicer and
+//    use the AlbumsListLoader component
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header as='h1'>Hello World!</Header>
-      </div>
+      <Grid padded>
+        <Grid.Column>
+          <AlbumsListLoader />
+        </Grid.Column>
+      </Grid>
     );
   }
 }
